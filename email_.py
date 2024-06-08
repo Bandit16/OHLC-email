f6f6ff72b75791d1f8446a20c85cb1ce7028bfe0
Dipesh Acharya <dipeshacharya87@gmail.com>
2024-06-08 13:04:15 +0545

    modified loads

--- email_.py
+++ email_.py
@@ -8,8 +8,8 @@ from email.mime.base import MIMEBase
 
 def send_email():
     # Email account credentials
-    from_email = "youremail@gmail.com"
-    password = "your app password here"  
+    from_email = "sendermail@gmail.com"
+    password = "your app password"  
     # make changes here
     to_email = "receivermail@gmail.com"
     # Replace with your actual credentials (avoid putting them in code)
@@ -24,15 +24,24 @@ def send_email():
 
     msg.attach(MIMEText(body, "plain"))
 
-    with open('stock_chart.png', "rb") as attachment_file:
+    with open('data.xlsx', "rb") as attachment_file:
         attachment_part = MIMEBase("application", "octet-stream")
         attachment_part.set_payload(attachment_file.read())
 
     encoders.encode_base64(attachment_part)
 
-    attachment_part.add_header("Content-Disposition", f"attachment; filename={'stock_chart.png'.split('/')[-1]}")
+    attachment_part.add_header("Content-Disposition", f"attachment; filename={'data.xlsx'.split('/')[-1]}")
+
+    with open('stock_chart.png', "rb") as attachment_file:
+        attachment_part2 = MIMEBase("application", "octet-stream")
+        attachment_part2.set_payload(attachment_file.read())
+
+    encoders.encode_base64(attachment_part2)
+
+    attachment_part2.add_header("Content-Disposition", f"attachment; filename={'stock_chart.png'.split('/')[-1]}")
 
     msg.attach(attachment_part)
+    msg.attach(attachment_part2)
 
     try:
         # Create server object with SSL option
@@ -45,5 +54,5 @@ def send_email():
         print(f"Email sent successfully to {to_email}.")
     except Exception as e:
         print(f"Failed to send email. Error: {e}")
-
+send_email()
 
